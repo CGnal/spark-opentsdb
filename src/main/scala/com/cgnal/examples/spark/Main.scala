c1deda955f7e56e0a6b736ac92eef7625d09be99
David Greco <greco@acm.org>
2017-02-20 10:58:54 +0100

    changed the licens

--- src/main/scala/com/cgnal/examples/spark/Main.scala
+++ src/main/scala/com/cgnal/examples/spark/Main.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2016 CGnal S.p.A.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.cgnal.examples.spark
@@ -53,8 +64,6 @@ object Main extends App {
         setMaster("yarn-client").
         setAppName("spark-cdh5-template-yarn").
         setJars(List(uberJarLocation)).
-        set("spark.yarn.jar", "local:/opt/cloudera/parcels/CDH/lib/spark/assembly/lib/spark-assembly.jar").
-        set("spark.executor.extraClassPath", "/opt/cloudera/parcels/CDH/jars/*"). //This is an hack I made I'm not sure why it works though
         set("spark.serializer", "org.apache.spark.serializer.KryoSerializer").
         set("spark.io.compression.codec", "lzf").
         set("spark.speculation", "true").
